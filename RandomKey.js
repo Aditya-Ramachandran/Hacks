0d49c68c428a8105547ab721caf330362b5b0b5f
Aditya Ramachandran <adityaramachandran25@gmail.com>
2020-04-05 22:17:44 +0530

    Random Key Generator

---
+++ RandomKey.js
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html>
+<title>Random Key Generator</title>
+<head>
+<script>
+function RandomKey() {
+   document.write("Random Key :  ");
+
+   document.write(Date.now());
+}
+RandomKey();
+</script>
+</head>
+<body>
+</body>
